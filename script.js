ebf45c5578e34400f956715a7af67a8c4e5e85a2
Eduardo <nilaosornoe@gmail.com>
2021-12-31 15:00:23 -0600

    Firt commit, Slime Game

---
+++ script.js
@@ -0,0 +1,53 @@
+var puntos=0;
+var segundos=8;
+
+function inicia(){
+  setTimeout(fin, segundos * 1000);
+}
+
+
+
+
+function posicion(){
+
+  var enemigo = document.getElementById("enemigo");
+  enemigo.style.position="absolute";
+  x=Math.floor((Math.random()*900)+1);
+  y=Math.floor((Math.random()*900)+1);
+  enemigo.style.left=x + "px";
+  enemigo.style.top=y + "px";
+
+
+ puntos++;
+}
+
+
+
+function posicision(){
+
+  var enemigo = document.getElementById("enemigo_2");
+  enemigo.style.position="absolute";
+  x=Math.floor((Math.random()*1200)+1);
+  y=Math.floor((Math.random()*1200)+1);
+  enemigo.style.left=x + "px";
+  enemigo.style.top=y + "px";
+
+
+ puntos+=40;
+
+
+}
+
+function fin(){
+
+ alert("Game over- Puntos: " + puntos);
+}
+
+
+var Countdown = setInterval(function(){
+--segundos;
+document.getElementById("contadorTexto").innerHTML = segundos;
+if(segundos <= 0){
+segundos = 3;
+}
+}, 1000);
